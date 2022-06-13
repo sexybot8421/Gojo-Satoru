33c2a5c085a14e176ebef03f13bd80e3efd93a0b
sexybot8421 <103559081+sexybot8421@users.noreply.github.com>
2022-06-13 12:53:16 +0530

    Update Gojosensei.js

--- Gojosensei.js
+++ Gojosensei.js
@@ -1776,7 +1776,7 @@ break
                                     id: 'owner'
                                 }
                             }]
-                      let txt = `「 GOJO-SATORU HERE🎉 」\n\n${text}`
+                      let txt = `「 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸 HERE🎉 」\n\n${text}`
                       GojoMdNx.send5ButImg(i, txt, GojoMdNx.user.name, global.thumb, btn)
                     }
                 reply(`Successful Sending Broadcast To ${anu.length} Group(s)`)
@@ -1810,7 +1810,7 @@ break
                                     id: 'owner'
                                 }
                             }]
-                      let txt = `「 GOJO-SATORU HERE 🎉」\n\n${text}`
+                      let txt = `「 𝚀𝚄𝙴𝙴𝙽 𝙰𝚂𝙷𝙸 HERE 🎉」\n\n${text}`
                       GojoMdNx.send5ButImg(yoi, txt, GojoMdNx.user.name, global.thumb, btn)
 		}
 		reply('Broadcast Success')
